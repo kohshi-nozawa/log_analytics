7ad2af588f6d180aa23bf2b44858119004d09500
kohshi.nozawa <k.nozawa@accenture.com>
2020-02-19 13:55:33 +0900

    cat log

--- bin/marge_log.sh
+++ bin/marge_log.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -u
 
-date=`date '+%Y%m%d'`
-
-for file in `ls $date`
-do
-  name=`ls $date/$file | cut -c 10-`
-  while read line
-  do
-    echo $name"|"$line >> $date/marge$date.log
-  done < $date/$file
-done
+cat $1/*.log > $1/marge_log@$1.csv
 
 if [ -z `which nkf` ]; 
   brew install nkf
-  nkf -s --overwrite $date/marge$date.log
+  nkf -s --overwrite $1/marge_log@$1.csv
 else
-  nkf -s --overwrite $date/marge$date.log
+  nkf -s --overwrite $1/marge_log@$1.csv
 fi
